d7741604b9957d075366be9120cc6e0758b1e463
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-08-24 04:40:02 +0300

    test

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -20,15 +20,16 @@ class FIFOCache(BaseCaching):
         """ Add an item in the cache
         """
         if key and item:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
-                    and key not in self.cache_data:
-                discard = self.queue.pop(0)
-                del self.cache_data[discard]
-                print("DISCARD: {}".format(discard))
+            if key in self.cache_data:
                 self.cache_data[key] = item
+                self.queue.remove(key)
             else:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    discard = self.queue.pop(0)
+                    del self.cache_data[discard]
+                    print("DISCARD: {}".format(discard))
                 self.cache_data[key] = item
-                self.queue.append(key)
+            self.queue.append(key)
 
     def get(self, key):
         """ Get an item by key
