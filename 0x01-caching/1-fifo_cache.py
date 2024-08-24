00b811a029f4deb7cfc72f68bce00d3ff3fb3314
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-08-24 04:39:18 +0300

    test

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" FIFO cache """
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache defines:
+      - Inherits from BaseCaching
+      - Caching system
+    """
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data:
+                discard = self.queue.pop(0)
+                del self.cache_data[discard]
+                print("DISCARD: {}".format(discard))
+                self.cache_data[key] = item
+            else:
+                self.cache_data[key] = item
+                self.queue.append(key)
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key, None)
